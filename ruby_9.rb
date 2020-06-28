a8a34166fe8ad118085949d4374ae26674783293
natsumerika <natsumesansan@gmail.com>
2020-06-28 18:15:51 +0900

    [update]

---
+++ ruby_9.rb
@@ -0,0 +1,30 @@
+# ruby_9.rb
+#メソッド自分で定義
+# def hello
+# 	"hello_111"
+	
+# end
+
+# puts hello
+
+# def greeting(name)
+#   "Hello, #{name}!" # nameの中には、文字列'John'が入っている
+# end
+
+
+# puts greeting('John')
+
+def hello(aa)
+	"hello,#{aa}"
+end
+
+
+# returnメソッド　戻り値
+puts hello("bbb")
+
+def hello
+	return "aaa"
+	"ddd"
+end
+
+puts hello
