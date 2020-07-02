9c9e1355823b40bbe4d1bfc0f59429cc186965a2
natsumerika <natsumesansan@gmail.com>
2020-07-02 22:43:48 +0900

    [ceate]rails.rb

---
+++ Rails/rails.rb
@@ -0,0 +1,12 @@
+# rals
+
+# バリデーション チェック
+validates :title, presence: :true
+
+<%= if @post_images.error.any? %>
+	<%= @post_images.error.count %>件があります
+	# 間違えやすい
+	<% @post_images.error.full_.messages.each do |message| %>
+		<%= message %>
+	<% end %>
+<% end %>
